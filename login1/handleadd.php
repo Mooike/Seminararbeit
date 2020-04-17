f7456adba6fc5180f124e8c39f7c1d70dc1b127f
Mookie <mleins60@gmail.com>
2020-04-17 13:31:01 +0200

    test

---
+++ login1/handleadd.php
@@ -0,0 +1,37 @@
+<!DOCTYPE HTML>
+<html>
+  
+  <head>
+    <title>Login-Mechanismus</title>
+  </head>
+  
+  <body>
+
+	
+  </body>
+  
+</html>
+
+
+
+
+<?php 
+ $benutzer = $_POST["Benutzer"];
+ $passwort = $_POST["Passwort"];
+
+
+
+ $mysqli = new mysqli('localhost', 'test', '123', 'anmeldung');
+ if ($mysqli->connect_errno) {
+  die('Verbindungsfehler (' . $mysqli->connect_errno. ') ' . $mysqli->connect_error);
+ }
+
+  $sql = "INSERT INTO `benutzer`(`Benutzername`, `Passwort`) VALUES ('$benutzer','$passwort')";
+  $result = $mysqli->query($sql);
+  echo "Erfolgreich registriert!";
+
+
+$mysqli->close();
+
+?>
+
