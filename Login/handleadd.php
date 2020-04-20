87ce891b740c5b80c37f78dd3efb736b9cc47436
Mookie <mleins60@gmail.com>
2020-04-20 20:03:39 +0200

    2

--- Login/handleadd.php
+++ Login/handleadd.php
@@ -20,22 +20,16 @@ require 'rb.php';
  $benutzer = $_POST["Benutzer"];
  $passwort = password_hash($_POST["Passwort"], PASSWORD_DEFAULT);//hash password
 
-
- R::setup('mysql:host=localhost;dbname=anmeldung', 'test', '123');
- //$mysqli = new mysqli('localhost', 'test', '123', 'anmeldung');
- //if ($mysqli->connect_errno) {
-  //die('Verbindungsfehler (' . $mysqli->connect_errno. ') ' . $mysqli->connect_error);
- //}
+ R::setup('mysql:host=localhost;dbname=login', 'root', '');
   $b = R::dispense('benutzer'); 
-  //$sql = "INSERT INTO `benutzer`(`Benutzername`, `Passwort`) VALUES ('$benutzer','$passwort')";
+  
   $b->Benutzername = $benutzer;
   $b->Passwort = $passwort;
-  //$result = $mysqli->query($sql);
-  echo "Erfolgreich registriert!";
+
   $id = R::store($b);
 
   R::close();
-//$mysqli->close();
+
 
 ?>
 
